8e03fcd2c67240de1eaed6c0fed8b4e9e99408cf
Dinesh <dineshkrishnareddy@gmail.com>
2014-09-29 09:40:47 +0530

    Final Signup

--- Signup/Signup.bat
+++
@@ -1,4 +0,0 @@
-cd %cd%
-SET ANT_HOME=%cd%\libs\apache-ant-1.9.4
-SET PATH=%ANT_HOME%\bin
-ant
--- Signup/src/test/Main.java
+++ Signup/src/test/Main.java
@@ -4,7 +4,7 @@ package test;
 public class Main {
     public static void main(String[] args) {
     	try{
-    		Runtime.getRuntime().exec("cmd /c start Signup.bat");
+    		Runtime.getRuntime().exec("cmd /c start testng.bat");
     	}catch (Exception e) {
     		System.out.println("Exception");
 		}
